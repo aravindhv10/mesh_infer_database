ccee1cd56767f9c4cda088dd7afc357b09436e08
asd <aravindhv10@gmail.com>
2026-05-07 22:08:50 +0530

    More refinement of git scripts

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,29 @@
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    match args.len() {
+        0 => {
+            eprintln!("Bad command line array");
+        }
+        1 => {
+            eprintln!("Need atleast 1 commandline argument {}", args[0]);
+        }
+        _ => {
+            match args[1].as_str() {
+                "send" => {
+                    eprint!("Operating on send mode");
+                }
+                "receive" => {
+                    eprint!("Operating on receive mode");
+                }
+                _ => {
+                    eprint!("Unknown mode");
+                }
+            };
+        }
+    };
+
     println!("Hello, world!");
 }
